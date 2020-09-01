dea1011cbe9da10e4771e22e0bc21ae4f1545d5a
Anna <anna_agabekyan_anna@mail.ru>
2020-09-01 20:45:20 +0400

    feat: build sidebar

--- whatsapp/src/App.js
+++ whatsapp/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
+import Sidebar from './Sidebar'
 
 function App() {
   return (
     <div className="App">
-     
+      <div className="app_body">
+        {/* sidebar */}
+        <Sidebar />
+        {/* chat */}
+      </div>
     </div>
   );
 }
