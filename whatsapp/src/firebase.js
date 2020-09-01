1d851df98d1d9b8f8995054a4a3b3fe02720bcea
Anna <anna_agabekyan_anna@mail.ru>
2020-09-01 17:38:22 +0400

    feat: start project w/a

---
+++ whatsapp/src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDe6NANCmvxA1gKS9rar7UXNqKKH1PXVTw",
+    authDomain: "whatsapp-d4127.firebaseapp.com",
+    databaseURL: "https://whatsapp-d4127.firebaseio.com",
+    projectId: "whatsapp-d4127",
+    storageBucket: "whatsapp-d4127.appspot.com",
+    messagingSenderId: "909685229985",
+    appId: "1:909685229985:web:e590157092b43a515563e0",
+    measurementId: "G-YXFBWR1RZG"
+  };
